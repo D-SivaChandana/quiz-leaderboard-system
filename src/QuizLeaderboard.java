65e1c39e4154ca937475c09e3d5692bd02a45e25
Dwarampudi Siva Chandana <chandanareddy562@gmail.com>
2026-04-24 18:29:42 +0530

    QuizLeaderboard.java

--- src/QuizLeaderboard.java
+++ src/QuizLeaderboard.java
@@ -1,1 +1,27 @@
+import java.net.http.*;
+import java.net.URI;
+import java.util.*;
 
+public class QuizLeaderboard {
+    public static void main(String[] args) throws Exception {
+
+        Set<String> seen = new HashSet<>();
+        Map<String, Integer> scores = new HashMap<>();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        for (int i = 0; i < 10; i++) {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://devapigw.vidalhealthtpa.com/srm-quiz-task/quiz/messages?regNo=2024CS101&poll=" + i))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.body()); // (for testing)
+
+            Thread.sleep(5000);
+        }
+    }
+}
